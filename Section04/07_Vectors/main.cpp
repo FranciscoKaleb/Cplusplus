d65cf5c47fd7c57ea51797add6068c3ed45f7ca8
francisco <franciscokaleb.marquez02@gmail.com>
2022-12-29 16:48:36 +0700

    added vector challenge exercise at 07

--- Section04/07_Vectors/main.cpp
+++ Section04/07_Vectors/main.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+/*
 int main(){
     //creating an string vector
     vector<string> item_slot {"boots","shovel","scroll"};
@@ -70,5 +71,42 @@ int main(){
     cout << movie_ratings.at(0).at(4) << endl;
     cout << movie_ratings.at(1).at(1) << endl;
     
+    
+}
+
+*/
+// expand code above to see lesson examples
+// below is exercise 
+int main(){
+    
+    vector<int> vector1{};
+    vector<int> vector2{};
+    
+    vector1.push_back(10);
+    vector1.push_back(20);
+    cout << vector1.size() << endl;
+    cout << vector1.at(0) << endl;
+    cout << vector1.at(1) << endl;
+    
+    vector2.push_back(100);
+    vector2.push_back(200);
+    cout << vector2.size() << endl;
+    cout << vector2.at(0) << endl;
+    cout << vector2.at(1) << endl;
+    
+    vector<vector<int>> vector_2d {};
+    
+    vector_2d.push_back(vector1);
+    vector_2d.push_back(vector2);
+    
+    cout << vector_2d.at(0).at(0) << endl;
+    cout << vector_2d.at(0).at(1) << endl;
+    cout << vector_2d.at(1).at(0) << endl;
+    cout << vector_2d.at(1).at(1) << endl;
+    
+    vector1.at(0) = 1000;
+    
+    cout << vector1.at(0) << endl;
+    cout << vector1.at(1) << endl;
     
 }
