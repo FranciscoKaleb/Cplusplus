1ab02e1be503e12cfc14bdbb83a74ea8e0da6e99
francisco <franciscokaleb.marquez02@gmail.com>
2022-12-30 19:04:06 +0700

    logical operator challenge done

--- Section04/12_LogicalOperators/main.cpp
+++ Section04/12_LogicalOperators/main.cpp
@@ -39,6 +39,7 @@ public:
     void Example_3()
     {
         int num {0};
+
         const int lower_one {10};
         const int upper_one {20};
         const int lower_two {50};
@@ -52,13 +53,55 @@ public:
         boundary = (((num > 10 ) && (num < 20)) || ((num > 50) && (num < 60)));
         cout << "within boundary?" << boundary ;
     }
+    void Challenge()
+    {
+         /*
+         1 dollar  = 100 cents
+         1 quarter = 25 cents
+         1 dime = 10 cents
+         1 nickel = 5 cents
+         1 penny = 1 cent
+         */
+         const int dollar_to_cent {100};
+         const int quarter_to_cent {25};
+         const int dime_to_cent {10};
+         const int nickel_to_cent {5};
+         const int penny_to_cent {1};
+         
+         int dollar {0};
+         int quarter {0};
+         int dime {0};
+         int nickel {0};
+         int penny {0};
+         
+         int num {0};
+         int remainder_placeholder {0};
+         
+         cout << "Enter the number of cents: "  << endl;
+         cin >> num;
+         
+         dollar = num/dollar_to_cent; // assign value to dollar by dividing input to 100
+         remainder_placeholder = num%dollar_to_cent; //get the remainder
+         quarter = remainder_placeholder/quarter_to_cent; //make the remainder as the new numerator in place of 'num'
+         remainder_placeholder = remainder_placeholder%quarter_to_cent;//get the remainder;
+         dime  = remainder_placeholder/dime_to_cent;
+         remainder_placeholder = remainder_placeholder%dime_to_cent;
+         nickel = remainder_placeholder/nickel_to_cent;
+         remainder_placeholder = remainder_placeholder%nickel_to_cent;
+         penny = remainder_placeholder/penny_to_cent;
+         
+         
+         cout << dollar << endl << quarter << endl << dime << endl << nickel << endl << penny;
+         
+
+    }
 };
 
 int main()
 {
     
     Examples obj;
-    obj.Example_3();
+    obj.Challenge();
     
     
 }
